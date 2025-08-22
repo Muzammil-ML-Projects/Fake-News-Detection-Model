b150257ed420dab59e006ee172c6d42b0ba3cdb4
m-muzammil-irshad <cornerofcodes00@gmail.com>
2025-08-22 21:58:17 +0500

    Some adjustments in html, css & .py file

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import joblib,re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+# Load model and vectorizer
 model = joblib.load("model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
+# Fn to clean the news text
 def textCleaning(text):
     text = text.lower()
     text =  re.sub(r'\S*[\\/]\S*', "", text)
@@ -14,18 +16,22 @@ def textCleaning(text):
     text = re.sub(r'\s+', ' ', text)
     return text
 
+# Fn to make tokens
 def wordTokenization(text):
     token = word_tokenize(text)
     stop_words = set(stopwords.words("english"))
     tokenize_text = [word for word in token if word not in stop_words]
     return tokenize_text
 
+# app object
 app = Flask(__name__)
 
+# To render index.html file
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# To make prediction and render it
 @app.route("/predict", methods = ["POST"]) 
 def predict():
     news = request.form['news']
@@ -37,5 +43,6 @@ def predict():
     result = "Real" if prediction == 1 else "Fake"
     return render_template("index.html", prediction=result)
 
+# Execute
 if __name__ == "__main__":
     app.run(debug=True)
